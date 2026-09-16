82d2ca32753949821b978d1bd7e3fa6555d0c50b
Freyja <freyamarika@gmail.com>
2026-09-16 18:58:01 +1000

    Enhance pathfinding with smoothing and visibility checks
    Added `SmoothPath` and `IsVisibleFrom` methods to `NodeMap` to enable path smoothing and visibility checks. Updated `PathAgent` to use `NodeMap` for path smoothing and added diagonal node connections for more realistic movement. Simplified `GetPath` logic in `PathAgent` and `Agent`. Fixed memory allocation issue in `NodeMap` and included `raylib.h` for drawing support.

--- AIE_Starter/NodeMap.h
+++ AIE_Starter/NodeMap.h
@@ -20,6 +20,8 @@ namespace AIForGames
         void Initialise(std::vector<std::string> asciiMap, int cellSize);
         void Draw(bool shouldDraw);
         void DrawPath(std::vector<Node*> path, Color lineColor);
+		std::vector<Node*> SmoothPath(std::vector<Node*> path);
+        bool IsVisibleFrom(Node* start, Node* end);
         Node* GetNode(int x, int y);
         Node* GetClosestNode(glm::vec2 worldPos);
         Node* GetRandomNode();
